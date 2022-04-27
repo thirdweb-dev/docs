f35faaa8bf6dd98d483d05553f87e78bca4a806a
adam-maj <64697628+adam-maj@users.noreply.github.com>
2022-04-27 01:59:39 -0400

    am/python (#25)
    * Update snippets to have python
    
    * update docs

--- scripts/generate-snippets.mjs
+++ scripts/generate-snippets.mjs
@@ -20,6 +20,10 @@ const react = JSON.parse(
   fs.readFileSync(`${process.cwd()}/submodules/react/docs/snippets.json`, "utf8"),
 );
 
+const python = JSON.parse(
+  fs.readFileSync(`${process.cwd()}/submodules/python/docs/docs/snippets.json`, "utf8"),
+);
+
 const snippets = CONTRACTS.reduce((acc, contractName) => {
   const data = {
     name: contractName,
@@ -33,14 +37,40 @@ const snippets = CONTRACTS.reduce((acc, contractName) => {
   // Get snippets from every SDK
   const tsExample = Object.values(typescript).find(snippet => snippet.name.toLowerCase() === contractName.toLowerCase());
   const reactExample = Object.values(react).find(snippet => snippet.name.toLowerCase().includes(contractName.toLowerCase()));
-  const pythonExample = undefined;
+  const pythonExample = Object.values(python).find(snippet => snippet.name.toLowerCase().includes(contractName.toLowerCase()));
 
   // Get contract summary from typescript docs
   data.summary = tsExample?.summary || "";
 
-  // Get snippets for methods and properties
-  data.methods = tsExample?.methods || [];
-  data.properties = tsExample?.properties || [];
+  // Get snippets for methods
+  data.methods = tsExample?.methods?.map(method => ({
+    name: method.name,
+    summary: method.summary,
+    remarks: method.remarks,
+    examples: {
+      javascript: tsExample?.methods?.find(m => m.name.toLowerCase() === method.name.toLowerCase())?.examples?.javascript || "",
+      python: pythonExample?.methods?.find(m => m.name.replaceAll("_", "").toLowerCase() === method.name.toLowerCase())?.example || "",
+    },
+    reference: {
+      javascript: tsExample?.methods?.find(m => m.name.toLowerCase() === method.name.toLowerCase())?.reference || "",
+      python: pythonExample?.methods?.find(m => m.name.replaceAll("_", "").toLowerCase() === method.name.toLowerCase())?.reference || "",
+    }
+  })) || [];
+
+  // Get snippets for properties
+  data.properties = tsExample?.properties?.map(property => ({
+    name: property.name,
+    summary: property.summary,
+    remarks: property.remarks,
+    examples: {
+      javascript: tsExample?.properties?.find(p => p.name.toLowerCase() === property.name.toLowerCase())?.examples?.javascript || "",
+      python: pythonExample?.properties?.find(p => p.name.replaceAll("_", "").toLowerCase() === property.name.toLowerCase())?.example || "",
+    },
+    reference: {
+      javascript: tsExample?.properties?.find(p => p.name.toLowerCase() === property.name.toLowerCase())?.reference || "",
+      python: pythonExample?.properties?.find(p => p.name.replaceALl("_", "").toLowerCase() === property.name.toLowerCase())?.reference || "",
+    }
+  })) || [];;
   
   // Add reference for typescript contract interface
   if (tsExample.reference) {
@@ -51,6 +81,7 @@ const snippets = CONTRACTS.reduce((acc, contractName) => {
   data.examples = {
     ...(tsExample?.examples || {}),
     ...(reactExample?.examples?.javascript ? { react: reactExample.examples.javascript } : {}),
+    ...(pythonExample?.example ? { python: pythonExample.example } : {}),
   }
 
   acc[contractName] = data
