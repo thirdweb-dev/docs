edfcba3aba87aeaaa7ff73d0e41badb2e6e6ce01
Jonas Daniels <jonas.daniels@outlook.com>
2022-05-10 14:06:23 -0700

    remove webpack cache via plugin

---
+++ plugins/disable-cache-plugin.js
@@ -0,0 +1,19 @@
+// A JavaScript function that returns an object.
+// `context` is provided by Docusaurus. Example: siteConfig can be accessed from context.
+// `opts` is the user-defined options.
+async function disableCachePlugin(context, opts) {
+  return {
+    name: "docusaurus-disable-cache-plugin",
+
+    configureWebpack(config, isServer, utils, content) {
+      // Modify internal webpack config. If returned value is an Object, it
+      // will be merged into the final config using webpack-merge;
+      // If the returned value is a function, it will receive the config as the 1st argument and an isServer flag as the 2nd argument.
+
+      //just disable the webpack cache, as it takes more time than just re-building every time
+      return { cache: false };
+    },
+  };
+}
+
+module.exports = disableCachePlugin;
