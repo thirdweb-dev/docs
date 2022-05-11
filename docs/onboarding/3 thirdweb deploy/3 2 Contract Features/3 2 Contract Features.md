7849f1408a1bde067d063fed5172e888c7eed199
adam-maj <mr.adam.maj@gmail.com>
2022-05-11 15:33:33 -0400

    Add feature code snippets

--- docs/onboarding/3 thirdweb deploy/3 2 Contract Features/3 2 Contract Features.md
+++ docs/onboarding/3 thirdweb deploy/3 2 Contract Features/3 2 Contract Features.md
@@ -3,6 +3,10 @@ slug: /thirdweb-deploy/contract-features
 title: Contract Features
 ---
 
-# How to unlock SDK and Dashboard features
+# How To Unlock SDK and Dashboard Features
 
-// TODO explain high level reasoning
+The magic of the thirdweb deploy toolset is that you can implement specific features in your contracts to unlock full support for those features in the SDKs and the thirdweb Dashboard.
+
+By implementing features from our [Contracts SDK](https://docs.thirdweb.com/contracts), we automatically give you full SDKs to support your contracts as well as custom-made interfaces to interact with your contracts in the dashboard.
+
+You can check out each specific interface page to see how to implement the interfaces in your contracts and to learn what features you get for implementing these interfaces.
---
+++ src/components/FeatureCodeSnippets.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+// @ts-ignore
+import jsonData from "../../docs/feature_snippets.json";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
+import CodeBlock from "@theme/CodeBlock";
+
+export default function FeatureCodeSnippets({ 
+  featureName
+}) {
+  if (!featureName) {
+    return null;
+  }
+
+  const featureObject = jsonData[featureName];
+
+  if (!featureObject) {
+    return null;
+  }
+
+  const { summary, examples, methods, reference: references } = featureObject;
+
+
+  return (
+    <>
+      <CodeSnippet examples={examples} references={references} />
+    </>
+  );
+}
+
+function CodeSnippet({ examples, references }) {
+  const languageToHighlightMapping = {
+    javascript: "typescript",
+    python: "python",
+    react: "jsx",
+  };
+
+  return (
+    <Tabs
+      groupId="thirdweb-code-snippet"
+      defaultValue={Object.keys(examples)[0]}
+    >
+      {Object.keys(examples).map((language) => {
+        const example = examples[language];
+        const reference = references[language];
+        // Capitalize first letter for language name
+        const languageName =
+          language.charAt(0).toUpperCase() + language.slice(1);
+
+        // If the example is empty, return null
+        if (!example) {
+          // @ts-ignore
+          return <TabItem value={""}></TabItem>;
+        }
+
+        return (
+          <TabItem key={language} value={language} label={languageName}>
+            {/* <b>{summary}</b> */}
+            <CodeBlock language={languageToHighlightMapping[language]}>
+              {example}
+            </CodeBlock>
+
+            {reference && (
+              <a
+                href={reference}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  display: "block",
+                  marginTop: "1rem",
+                  textDecoration: "none",
+                }}
+              >
+                View in {languageName} SDK Documentation
+              </a>
+            )}
+          </TabItem>
+        );
+      })}
+    </Tabs>
+  )
+}
