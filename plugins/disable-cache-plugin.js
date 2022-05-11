083c23ba3fbf840769ff494a1e04b1bf6446eaa1
Jonas Daniels <jonas.daniels@outlook.com>
2022-05-11 16:20:56 -0700

    disable cache (#40)

--- plugins/disable-cache-plugin.js
+++ plugins/disable-cache-plugin.js
@@ -15,6 +15,10 @@ async function disableCachePlugin(context, opts) {
       // will be merged into the final config using webpack-merge;
       // If the returned value is a function, it will receive the config as the 1st argument and an isServer flag as the 2nd argument.
 
+      if (config.mode === "development") {
+        //don't disable cache in dev mode
+        return {};
+      }
       //just disable the webpack cache, as it takes more time than just re-building every time
       return { cache: false };
     },
