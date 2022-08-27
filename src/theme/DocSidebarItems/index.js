9fd2867d4f587d5aa3619ddeabff6a2973242c16
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2022-08-27 11:34:35 +1000

    Jw/sidebar idea (#259)
    * Fix import snippet tracking
    
    * broken thirdweb cli links, fix target blank dont open in new tab unless external
    
    * Add hover effects to template cards
    
    * Fix broken react generation script
    
    * Build & Update Edition Drop Template
    
    * Renames: 1. Contracts SDK -> Extensions 2. Web3 SDK -> SDK
    
    * Sidebar sections idea WIP
    
    * Portal Overview Images
    
    * Better responsive image styling on hero grid on platform over view page
    
    * fix images font weight platform overview. More work on sidebar
    
    * Fix links to /cli
    
    * Move create up
    
    * Address some feedback

---
+++ src/theme/DocSidebarItems/index.js
@@ -0,0 +1,98 @@
+import React, { memo } from "react";
+import { DocSidebarItemsExpandedStateProvider } from "@docusaurus/theme-common/internal";
+import DocSidebarItem from "@theme/DocSidebarItem";
+// TODO this item should probably not receive the "activePath" props
+// TODO this triggers whole sidebar re-renders on navigation
+function DocSidebarItems({ items, ...props }) {
+  const showCategories =
+    props?.level === 1 &&
+    !props?.activePath.startsWith("/python") &&
+    !props?.activePath.startsWith("/typescript") &&
+    !props?.activePath.startsWith("/react") &&
+    !props?.activePath.startsWith("/go") &&
+    !props?.activePath.startsWith("/contracts");
+
+  const sidebarItems = [
+    {
+      title: "",
+      items: [
+        "Home",
+        "Platform Overview",
+        "Getting Started",
+        "Create A Project",
+      ],
+    },
+    {
+      title: "Build Contracts",
+      items: ["Prebuilt", "Extensions"],
+    },
+    {
+      title: "Ship Projects",
+      items: ["Release", "Deploy"],
+    },
+    {
+      title: "Create Apps",
+      items: ["SDK", "Auth"],
+    },
+    {
+      title: "Manage Projects",
+      items: ["Dashboard"],
+    },
+    {
+      title: "",
+      items: ["Templates", "Guides", "SDK References"],
+    },
+  ];
+
+  if (showCategories) {
+    return (
+      <DocSidebarItemsExpandedStateProvider>
+        {sidebarItems.map((section, index) => (
+          <div key={section.title} className="sidebar-section-container">
+            {section.title && (
+              <p className="sidebar-section-title">{section.title}</p>
+            )}
+            {items
+              .filter((item) => section.items.includes(item.label))
+              .sort(
+                (a, b) =>
+                  section.items.indexOf(a.label) -
+                  section.items.indexOf(b.label),
+              )
+              .map((item, index) => (
+                <DocSidebarItem
+                  key={index}
+                  item={item}
+                  index={index}
+                  {...props}
+                />
+              ))}
+          </div>
+        ))}
+
+        {/* Now the rest of the items */}
+        {items
+          .filter(
+            (item) =>
+              !sidebarItems.some((section) =>
+                section.items.includes(item.label),
+              ),
+          )
+          .filter((item) => item.label !== "Guides")
+          .map((item, index) => (
+            <DocSidebarItem key={index} item={item} index={index} {...props} />
+          ))}
+      </DocSidebarItemsExpandedStateProvider>
+    );
+  }
+
+  return (
+    <DocSidebarItemsExpandedStateProvider>
+      {items.map((item, index) => (
+        <DocSidebarItem key={index} item={item} index={index} {...props} />
+      ))}
+    </DocSidebarItemsExpandedStateProvider>
+  );
+}
+// Optimize sidebar at each "level"
+export default memo(DocSidebarItems);
