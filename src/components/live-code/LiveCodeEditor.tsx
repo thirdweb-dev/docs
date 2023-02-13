108887a9a44c2f632411908c6bcee45eef59447b
Jarrod Watts <jarrodwatts16@gmail.com>
2023-02-13 17:10:13 +1100

    wip

--- src/components/live-code/LiveCodeEditor.tsx
+++ src/components/live-code/LiveCodeEditor.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import { useColorMode } from "@docusaurus/theme-common";
+import lightCodeTheme from "prism-react-renderer/themes/vsLight";
+import darkCodeTheme from "prism-react-renderer/themes/oceanicNext";
+
+console.log({ lightCodeTheme, darkCodeTheme });
 
 type Props = {
   code: string;
@@ -8,12 +13,36 @@ type Props = {
 };
 
 export default function ConnectWalletEditor({ code, scope }: Props) {
+  const { colorMode } = useColorMode();
+
   return (
     <ThirdwebProvider activeChain="goerli">
       <LiveProvider code={code} scope={scope}>
-        <LiveEditor />
-        <LiveError />
-        <LivePreview />
+        <div
+          style={{
+            border: "1px solid rgba(255,255,255,0.1)",
+            borderRadius: "8px",
+            padding: "8px",
+            display: "flex",
+            flexDirection: "row",
+            gap: "2%",
+            maxWidth: "95vw",
+          }}
+        >
+          <div style={{ width: "60%" }}>
+            <LiveEditor
+              about=""
+              theme={colorMode === "dark" ? darkCodeTheme : lightCodeTheme}
+              style={{
+                "--ifm-pre-background": "#282c34",
+              }}
+            />
+          </div>
+          <div style={{ width: "38%" }}>
+            <LivePreview />
+            <LiveError />
+          </div>
+        </div>
       </LiveProvider>
     </ThirdwebProvider>
   );
--- src/components/live-code/snippets/UseNFTHook.tsx
+++ src/components/live-code/snippets/UseNFTHook.tsx
@@ -9,15 +9,16 @@ const code = `function App() {
   // The token ID of the NFT you want to fetch
   const tokenId = 0;
 
-  const { contract } = useContract("0x149ec8910A8e699D708f2b9c75e1F864DfCD1e1c");
+  const { contract } = useContract("0x4935154046C2f281AcE726018149F1a25449910d");
   const { data: nft, isLoading, error } = useNFT(contract, tokenId);
 
   if (isLoading) return <div>Fetching NFT…</div>;
   if (error) return <div>Error fetching NFT</div>;
   if (!nft) return <div>NFT not found</div>;
-  return <div>
-    <strong>NFT Metadata</strong>
-    <pre>{JSON.stringify(nft, null, 2)}</pre>
+  
+  return <div style={{display:'flex', flexDirection:'column'}}>
+    <strong>{nft.metadata.name}</strong>
+    <img src={nft.metadata.image}/>
   </div>;
 }`;
 
