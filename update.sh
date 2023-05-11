6a350908ad086f9f7c95e07324add0519d70cbfe
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2023-05-11 12:43:36 +1000

    Use pnpm for update.sh action (#668)
    * Fix update.sh action
    
    * Make update.sh use pnpm instead of yarn

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ git submodule init
 git submodule update --remote
 git submodule foreach git checkout main
 git submodule foreach git pull origin main
-yarn install
+pnpm install
 # go into js package and build
 cd ./submodules/js
 pnpm install
@@ -32,7 +32,7 @@ if [ ! -d "./etc" ]; then
 fi
 pnpm generate-docs
 cd ../../../..
-yarn make-docs
-yarn generate-snippets
-yarn generate-github-examples
-yarn prettier
+pnpm make-docs
+pnpm generate-snippets
+pnpm generate-github-examples
+pnpm prettier
